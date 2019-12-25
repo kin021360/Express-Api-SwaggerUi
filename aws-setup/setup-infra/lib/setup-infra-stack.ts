e1df2d49886e24572c3240e4d87a582a796cbcdc
Nathan Lam - desktop <kin021360@yahoo.com.hk>
2019-12-25 22:20:38 +0800

    update

--- aws-setup/setup-infra/lib/setup-infra-stack.ts
+++ aws-setup/setup-infra/lib/setup-infra-stack.ts
@@ -9,16 +9,19 @@ export class SetupInfraStack extends cdk.Stack {
         super(scope, id, props);
         // The code that defines your stack goes here
 
-        const existingVpc = ec2.Vpc.fromLookup(this, "VPC", {vpcId: 'vpc-33113654'});
+        // Use existing VPC by VPC id
+        const existingVPC = ec2.Vpc.fromLookup(this, "VPC", {vpcId: 'vpc-33113654'});
 
         // Create an ECS cluster
         const cluster = new ecs.Cluster(this, 'Cluster', {
             clusterName: 'ecs-clusterX',
-            vpc: existingVpc
+            vpc: existingVPC
         });
 
+        // Create an ECS TaskDefinition
         const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef', {});
 
+        // Create an ECS container specs to TaskDefinition
         const container = taskDefinition.addContainer('web', {
             image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
             memoryLimitMiB: 256,
@@ -29,45 +32,50 @@ export class SetupInfraStack extends cdk.Stack {
             protocol: ecs.Protocol.TCP
         });
 
-        const sg = new ec2.SecurityGroup(this, 'sg-all', {
-            vpc: existingVpc
+        // Use Existing subnet by subnet id
+        const targetSubnet1 = ec2.Subnet.fromSubnetAttributes(this, 's1', {
+            availabilityZone: 'ap-northeast-1d', subnetId: 'subnet-0056be2b'
         });
-        sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
 
+        // Create ECS Service
         const ecsService = new ecs.FargateService(this, 'FargateService', {
             cluster,
-            taskDefinition, desiredCount: 0,
-            serviceName: 'FargateService-X',
+            taskDefinition,
+            desiredCount: 0,
             vpcSubnets: {
-                subnets: [ec2.Subnet.fromSubnetAttributes(this, 's1', {
-                    availabilityZone: 'ap-northeast-1d', subnetId: 'subnet-0056be2b'
-                })]
-            },
-            securityGroup: sg
+                subnets: [targetSubnet1]
+            }
         });
 
         // Create ALB
-        // const alb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
-        //     vpc: existingVpc,
-        //     internetFacing: true
-        // });
-        // const listener = alb.addListener('PublicListener', {port: 80, open: true});
-        //
-        // // Attach ALB to ECS Service
-        // listener.addTargets('ECS', {
-        //     port: 80,
-        //     targets: [ecsService.loadBalancerTarget({
-        //         containerName: 'web',
-        //         containerPort: 80
-        //     })],
-        //     // include health check (default is none)
-        //     healthCheck: {
-        //         interval: cdk.Duration.seconds(60),
-        //         path: "/health",
-        //         timeout: cdk.Duration.seconds(5),
-        //     }
-        // });
+        const alb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+            vpc: existingVPC,
+            internetFacing: true,
+            // securityGroup: sg
+        });
 
+        // Create ALB listener & addTargets
+        const listener = alb.addListener('PublicListener', {port: 80, open: true});
+        // Attach ALB to ECS Service
+        listener.addTargets('ECS', {
+            port: 80,
+            targets: [ecsService.loadBalancerTarget({
+                containerName: 'web',
+                containerPort: 80
+            })],
+            // include health check (default is none)
+            healthCheck: {
+                interval: cdk.Duration.seconds(60),
+                path: "/health",
+                timeout: cdk.Duration.seconds(5),
+            }
+        });
+
+        // Create SecurityGroup
+        // const sg = new ec2.SecurityGroup(this, 'sg-all', {
+        //     vpc: existingVPC
+        // });
+        // sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
     }
 
     addCfnOutput(id: string, description: string, value: string) {
